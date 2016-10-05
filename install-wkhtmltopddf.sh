0ef6a4b1bd7855b5b00261d410bdee6d5ac41ca8
Chris MacMackin <cmacmackin@gmail.com>
2016-10-05 15:06:20 +0100

    Changed install script for wkhtmltopdf binary.

--- install-wkhtmltopddf.sh
+++ install-wkhtmltopddf.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 sudo apt-get install -y openssl build-essential xorg libssl-dev
-wget http://wkhtmltopdf.googlecode.com/files/wkhtmltopdf-0.10.0_rc2-static-amd64.tar.bz2
-tar xvjf wkhtmltopdf-0.10.0_rc2-static-amd64.tar.bz2
-sudo chown root:root wkhtmltopdf-amd64
-sudo mv wkhtmltopdf-amd64 /usr/bin/wkhtmltopdf
+wget http://download.gna.org/wkhtmltopdf/0.12/0.12.3/wkhtmltox-0.12.3_linux-generic-amd64.tar.xz
+tar xvJf wkhtmltox-0.12.3_linux-generic-amd64.tar.xz
+sudo chown -R root:root wkhtmltox
+sudo cp -R wkhtmltox/* /usr/
